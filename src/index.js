21ab102006cf799c178741be85f2ad9244f6ca68
jacgit18 <joshuacarpentier21@gmail.com>
2020-09-21 06:06:53 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import BoardSwitcher from './BoardSwitcher';
 import './index.css'
 
 
@@ -81,8 +80,5 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// ReactDOM.render(
-//   <BoardSwitcher numBoards={3} />,
-//   document.getElementById("root")
-// ); 
+
 
